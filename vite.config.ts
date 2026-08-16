dc35d60b25ad4e9fff0f1a7cde24f03d5a0e9f0a
ryeojj2007 <thanlinh2202@gmail.com>
2026-08-16 12:42:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base :'/Game-To-n-h-c/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
